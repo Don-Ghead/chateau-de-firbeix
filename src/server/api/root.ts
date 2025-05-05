94a54099539e10ee8d556083c885fedf5fa1bab1
luketparsons@gmail.com <luketparsons@clearswift.com>
2025-05-06 00:48:44 +0100

    fix: remove unused router files

--- src/server/api/root.ts
+++
@@ -1,8 +0,0 @@
-import { createTRPCRouter } from './trpc'
-import { contactRouter } from './routers/contact'
-
-export const appRouter = createTRPCRouter({
-  contact: contactRouter,
-})
-
-export type AppRouter = typeof appRouter 
